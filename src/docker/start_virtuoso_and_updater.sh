eb58ca1606d5cc1857de860ae45b17293dfad633
Nuno Freire <nfreire@gmail.com>
2024-11-20 16:15:43 +0000

    test

--- src/docker/start_virtuoso_and_updater.sh
+++ src/docker/start_virtuoso_and_updater.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -jar /opt/sparql-updater/sparql-updater.jar &
+java -jar /opt/sparql-updater/sparql-updater.jar  > /ingest/output.txt 2>&1 &
 /virtuoso-entrypoint.sh
--- src/main/java/europeana/sparql/updater/Scheduler.java
+++ src/main/java/europeana/sparql/updater/Scheduler.java
@@ -29,14 +29,14 @@ public class Scheduler {
 			sqlFolder.mkdir();
 		
 		EuropeanaDatasetFtpServer ftpServer=EuropeanaDatasetFtpServer.GENERAL_PUBLIC;
-		EuropeanaSparqlEndpoint sparqlEndpoint=EuropeanaSparqlEndpoint.TEST_RND2;
+		EuropeanaSparqlEndpoint sparqlEndpoint=EuropeanaSparqlEndpoint.TEST_LOCAL;
 		sparqlEndpoint.setDebug(true);
 		VirtuosoGraphManagerCl graphManager=new VirtuosoGraphManagerCl(isqlCommand, 1111, "dba", "pa", ttlFolder, sqlFolder);
 		
 		Updater updater=new Updater(sparqlEndpoint, ftpServer, graphManager);
 		UpdateReport report = updater.runTestUpdate(
-				"1", "10"
-				, "2058621"
+//				"1", "10", "2058621"
+				"222", "90901", "9200357", "1021", "2058703", "97", "2059511", "394", "169", "842", "2063621", "2048377", "2021110", "9200517", "91650", "91699", "0943110", "2051909", "995", "09902", "1008", "2048620", "411", "9200509", "577", "1095", "753", "457", "2048401", "2024918", "2032015", "902", "05816", "724", "712", "2021723", "10621", "2063629", "2048375", "2048128", "08534", "232", "925", "9200171", "2022361", "9200503", "101", "9200121", "1051", "9200412", "641", "2021719", "916124", "2021632", "916110", "532", "2023008", "183", "0943103", "1001", "2022718", "2023804", "58", "759", "2021802", "154", "2021648", "2021662", "2026113", "130", "2064201", "50", "2058815", "30", "328", "9200435", "9200573", "1097", "10501", "91698", "308", "2059515", "9200475", "2059513", "0940418", "477", "9200215", "765", "387", "921"
 				);
 		
 		System.out.println(report.print());
