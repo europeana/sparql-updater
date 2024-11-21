7ae02e278baa52a5c8e26dd690b2f2a8ec8cbf01
Nuno Freire <nfreire@gmail.com>
2024-11-21 15:35:20 +0000

    test

--- src/docker/load-edm.sql
+++ src/docker/load-edm.sql
@@ -2,7 +2,7 @@ checkpoint;
 
 delete from DB.DBA.load_list;
 
-ld_dir ('/usr/share/proj', 'edm-v527-160401.owl', 'http://www.europeana.eu/schemas/edm/');
+ld_dir (':/opt/virtuoso-opensource/vad', 'edm-v527-160401.owl', 'http://www.europeana.eu/schemas/edm/');
 
 rdf_loader_run();
 
