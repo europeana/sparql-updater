22447adc5cabd9305d85fc2727cde028c5665376
Patrick Ehlert <patrick.ehlert@europeana.eu>
2025-01-29 10:07:50 +0100

    Remove deleting transaction file

--- src/docker/start_virtuoso_and_updater.sh
+++ src/docker/start_virtuoso_and_updater.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# If a container restarts transactions may not be committed yet. Virtuoso will try to roll forward these transactions on
-# the next startup which can take a long time during which Virtuoso is not available (in some cases 10 minutes). This is
-# problematic in k8s. As a workaround we always delete the transaction file (see EA-4034).
-if test -f /database/virtuoso.trx; then
-  echo "Deleting Virtuoso transaction file..."
-  rm -f /database/virtuoso.trx
-fi
-
-
 # Start the updater in the background
 echo "Starting SPARQL updater..."
 java -jar /opt/sparql-updater/sparql-updater.jar &
