35d8cde4f15eb151547584294740d8ac12d2d527
Patrick Ehlert <patrick.ehlert@europeana.eu>
2025-08-28 11:21:37 +0200

    Add logging for affected dataset

--- src/main/java/europeana/sparql/updater/Dataset.java
+++ src/main/java/europeana/sparql/updater/Dataset.java
@@ -88,10 +88,10 @@ public class Dataset {
             state = State.MISSING;
         } else {
             setTimestampSparql(dsAtSparql.getTimestampSparql());
-            if (getId().startsWith("123")) {
-                LOG.trace("Dataset {} with FTP timestamp {} is in Virtuoso with timestamp {} and state {}",
+//            if (getId().startsWith("123")) {
+                LOG.info("Dataset {} with FTP timestamp {} is in Virtuoso with timestamp {} and state {}",
                         getId(), timestampFtp, timestampSparql, dsAtSparql.getState());
-            }
+            //}
             if (timestampSparql == null || dsAtSparql.getState() == State.CORRUPT)
                 state = State.CORRUPT;
             else if (timestampFtp.isAfter(timestampSparql))
@@ -99,9 +99,9 @@ public class Dataset {
             else
                 state = State.UP_TO_DATE;
         }
-        if (getId().startsWith("123")) {
-            LOG.trace("Dataset {} state is now {}", getId(), state);
-        }
+        //if (getId().startsWith("123")) {
+            LOG.info("Dataset {} state is now {}", getId(), state);
+        //}
         return (state != State.UP_TO_DATE);
     }
 
